6c67058c3a86f60e8285334de7f178310152c2df
cap-full-stack-nguallini <capfullstacknguallini@gmail.com>
2019-11-30 06:32:11 -0300

    .

--- house-data.js
+++ house-data.js
@@ -1,51 +1,38 @@
-function quitarNulos(item) {
-    if (item != null) {
-        return item;
-    } else {
-        return "";
-    }
+var membersOfTheHouse = [];
+
+for (var i = 0; i < data.results[0].members.length; i++) {
+    membersOfTheHouse.push(data.results[0].members[i]);
 }
 
+var app = new Vue({
+    el: "#app",
+    data: {
+        membersOfTheHouse: []
+    }
+});
+
 function generaTabla() {
+
+    var membersOfTheHouseShowns = [];
 
     var partyBox = Array.from(document.querySelectorAll("input[name=party_box]:checked")).map(elt => elt.value);
 
     var stateSelected = document.querySelector("#state_selected").value;
 
-    var tabla = "";
-
-    tabla += "<thead>\
-            <tr>\
-            <th scope='col'>Name</th>\
-            <th scope='col'>Party</th>\
-            <th scope='col'>State</th>\
-            <th scope='col'>Years in Office</th>\
-            <th scope='col'>% Votes w/ Party</th>\
-            </tr>\
-            </thead>\
-            <tbody>";
-    for (var i = 0; i < data.results[0].members.length; i++) {
-        tabla += "<tr>";
-        for (var j = 0; j < 1; j++) {
-            if ((partyBox[0] == data.results[0].members[i].party || partyBox[1] == data.results[0].members[i].party || partyBox[2] == data.results[0].members[i].party) && (stateSelected == "ALL" || data.results[0].members[i].state == stateSelected)) {
-                tabla += "<td><a href='" + quitarNulos(data.results[0].members[i].url) + "'>" + quitarNulos(data.results[0].members[i].first_name) + " " + quitarNulos(data.results[0].members[i].middle_name) + " " + quitarNulos(data.results[0].members[i].last_name) + "</a></td>\
-                    <td>" + quitarNulos(data.results[0].members[i].party) + "</td>\
-                    <td>" + quitarNulos(data.results[0].members[i].state) + "</td>\
-                    <td>" + quitarNulos(data.results[0].members[i].seniority) + "</td>\
-                    <td>" + quitarNulos(data.results[0].members[i].votes_with_party_pct + "%") + "</td>";
-            }
+    for (var i = 0; i < membersOfTheHouse.length; i++) {
+        if ((partyBox[0] == membersOfTheHouse[i].party || partyBox[1] == membersOfTheHouse[i].party || partyBox[2] == membersOfTheHouse[i].party) && (stateSelected == "ALL" || membersOfTheHouse[i].state == stateSelected)) {
+            membersOfTheHouseShowns.push(membersOfTheHouse[i]);
         }
-        tabla += "</tr>";
     }
-    tabla += "</tbody>";
 
-    return tabla;
+    app.membersOfTheHouse = membersOfTheHouseShowns;
+
 }
 
-document.getElementById("house-data").innerHTML = generaTabla();
+generaTabla()
 
 function updateUI() {
-    document.getElementById("house-data").innerHTML = generaTabla();
+    generaTabla();
 }
 
 $("#FilterForm").on("change", updateUI);
--- statistics.js
+++ statistics.js
@@ -1,14 +1,17 @@
-var statistics = {
-    "number_of_democrats": numberOfDemocrats,
-    "number_of_republicans": numberOfRepublicans,
-    "number_of_independents": numberOfIndependents,
-    "total_number_of_members": totalNumberOfMembers,
-    "average_votes_wich_party_pct_for_democrats": avgVotesWithPartyForDemocrats,
-    "average_votes_wich_party_pct_for_republicans": avgVotesWithPartyForRepublicans,
-    "average_votes_wich_party_pct_for_independents": avgVotesWithPartyForlistIndependents,
-    "total_average_votes_wich_party_pct": totalAvgVotesWichPartyPct,
-    "least_loyal_bottom_ten_percent_of_party": leastLoyalBottomTenPercentOfParty,
-    "most_loyal_top_ten_percent_of_party": mostLoyalTopTenPercentOfParty,
-    "least_engaged_bottom_ten_percent_attendance": leastEngagedBottomTenPercentAttendance,
-    "most_engaged_top_ten_percent_attendance": mostEngagedTopTenPercentAttendance
-}
+var app = new Vue({
+    el: "#app",
+    data: {
+        number_of_democrats: numberOfDemocrats,
+        number_of_republicans: numberOfRepublicans,
+        number_of_independents: numberOfIndependents,
+        total_number_of_members: totalNumberOfMembers,
+        average_votes_wich_party_pct_for_democrats: avgVotesWithPartyForDemocrats,
+        average_votes_wich_party_pct_for_republicans: avgVotesWithPartyForRepublicans,
+        average_votes_wich_party_pct_for_independents: avgVotesWithPartyForlistIndependents,
+        total_average_votes_wich_party_pct: totalAvgVotesWichPartyPct,
+        least_loyal_bottom_ten_percent_of_party: leastLoyalBottomTenPercentOfParty,
+        most_loyal_top_ten_percent_of_party: mostLoyalTopTenPercentOfParty,
+        least_engaged_bottom_ten_percent_attendance: leastEngagedBottomTenPercentAttendance,
+        most_engaged_top_ten_percent_attendance: mostEngagedTopTenPercentAttendance
+    }
+});
