857a3dacdb26a9f79145c8592b16739d418c9199
cap-full-stack-nguallini <capfullstacknguallini@gmail.com>
2019-11-30 06:06:32 -0300

    .

--- house-data.js
+++ house-data.js
@@ -8,6 +8,10 @@ function quitarNulos(item) {
 
 function generaTabla() {
 
+    var partyBox = Array.from(document.querySelectorAll("input[name=party_box]:checked")).map(elt => elt.value);
+
+    var stateSelected = document.querySelector("#state_selected").value;
+
     var tabla = "";
 
     tabla += "<thead>\
@@ -23,11 +27,13 @@ function generaTabla() {
     for (var i = 0; i < data.results[0].members.length; i++) {
         tabla += "<tr>";
         for (var j = 0; j < 1; j++) {
-            tabla += "<td><a href='" + quitarNulos(data.results[0].members[i].url) + "'>" + quitarNulos(data.results[0].members[i].first_name) + " " + quitarNulos(data.results[0].members[i].middle_name) + " " + quitarNulos(data.results[0].members[i].last_name) + "</a></td>\
+            if ((partyBox[0] == data.results[0].members[i].party || partyBox[1] == data.results[0].members[i].party || partyBox[2] == data.results[0].members[i].party) && (stateSelected == "ALL" || data.results[0].members[i].state == stateSelected)) {
+                tabla += "<td><a href='" + quitarNulos(data.results[0].members[i].url) + "'>" + quitarNulos(data.results[0].members[i].first_name) + " " + quitarNulos(data.results[0].members[i].middle_name) + " " + quitarNulos(data.results[0].members[i].last_name) + "</a></td>\
                     <td>" + quitarNulos(data.results[0].members[i].party) + "</td>\
                     <td>" + quitarNulos(data.results[0].members[i].state) + "</td>\
                     <td>" + quitarNulos(data.results[0].members[i].seniority) + "</td>\
                     <td>" + quitarNulos(data.results[0].members[i].votes_with_party_pct + "%") + "</td>";
+            }
         }
         tabla += "</tr>";
     }
@@ -37,3 +43,9 @@ function generaTabla() {
 }
 
 document.getElementById("house-data").innerHTML = generaTabla();
+
+function updateUI() {
+    document.getElementById("house-data").innerHTML = generaTabla();
+}
+
+$("#FilterForm").on("change", updateUI);
